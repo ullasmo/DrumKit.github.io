f222568ad0745684a73c5ae40c9fb83e5507dbdc
ullasmo <ullasa.me18@bmsce.ac.in>
2022-09-20 20:41:51 +0530

    Drum Kit website

---
+++ index.js
@@ -0,0 +1,47 @@
+for(let i=0;i<document.querySelectorAll(".drum").length;i++){
+    document.querySelectorAll("button")[i].addEventListener("click", function() { 
+        /*let audio = new Audio("sounds/tom-1.mp3");
+        audio.play();*/
+        let buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        
+    });
+}
+document.addEventListener("keypress", function(event){
+    makeSound(event.key);
+});
+function makeSound(key){
+    switch (key) {
+        case "a":
+            let tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "s":
+            let tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            break;
+        case "d":
+            let tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            break;
+        case "f":
+            let tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            break;
+        case "j":
+            let crash = new Audio("sounds/crash.mp3");
+            crash.play();
+            break;
+        case "k":
+            let kick = new Audio("sounds/kick-bass.mp3");
+            kick.play();
+            break;
+        case "l":
+            let snare = new Audio("sounds/snare.mp3");
+            snare.play();
+            break;
+        default:
+            alert("Only press keys shown on screen")
+    }
+
+}
